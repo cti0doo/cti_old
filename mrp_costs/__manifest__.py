fc8c93fca0da5297c6c4d9f320dc02a7f8bd3809
ejjimenezc <ejjimenezc@unal.edu.co>
2019-12-25 17:51:19 -0500

    Updated Real_Cost module's manifest

--- mrp_costs/__manifest__.py
+++ mrp_costs/__manifest__.py
@@ -10,14 +10,14 @@
         Long description of module's purpose
     """,
 
-    'author': "My Company",
+    'author': "CTI Ltda",
     'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.34',
+    'version': '13.1.0',
 
     # any module necessary for this one to work correctly
     'depends': ['base','mrp','mrp_account_enterprise'],
@@ -26,9 +26,5 @@
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    ]
 }
