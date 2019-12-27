549f06663e95c2e9e6b304a1aee37b5dfd6484d6
Edwin jimenez <ejjimenezc@unal.edu.co>
2019-12-27 11:40:31 -0500

    Added Function to update all ajusted cost from production

--- mrp_costs/models/models.py
+++ mrp_costs/models/models.py
@@ -6,18 +6,39 @@ class MrpProduction(models.Model):
     _inherit = 'mrp.production'
 
 
-    original_product_price = fields.Float(default=0.0)
+    std_cost = fields.Float(default=0.0, string='Standard cost')
 
-    real_cost = fields.Float(default=0.0)
+    prod_cost = fields.Float(default=0.0, string='Production cost')
 
+    adjusted_cost = fields.Float(default=0.0, string='Adjusted real cost')
+ 
     def get_report_values(self, proid):
-        productions = self.env['mrp.production'].search([('product_id', 'in', (proid,)), ('state', '=', 'done')])
+        productions = self.env['mrp.production'].search([('id', 'in', (proid,)), ('state', '=', 'done')])
         res = self.env['report.mrp_account_enterprise.mrp_cost_structure'].get_lines(productions)
         return res
 
+    #To-Do: Report get_lines() only get the original cost of the products.
+    def update_monthly_cost(self):
+        productions = self.env['mrp.production'].search([('state', '=', 'done')])
+        for prod in productions:
+            report_values = self.get_report_values(prod.id)
+            unit_cost = []
+            for line in report_values:
+                total_cost = line['total_cost']
+                mo_qty = line['mo_qty']
+                operations = line['operations']
+                opcost = 0.0
+
+                for row in operations:
+                    opcost = opcost + row[3] * row[4]
+                
+                unit_cost.append((total_cost + opcost) / mo_qty)
+            if unit_cost:
+                prod.adjusted_cost = unit_cost[0]
+
     def write(self, values):
 
-        if self.real_cost <= 0.0 and self.state == 'done':
+        if self.prod_cost <= 0.0 and self.state == 'done':
 
             report_values = self.get_report_values(self.id)
             unit_cost = []
@@ -28,11 +49,12 @@ class MrpProduction(models.Model):
                 opcost = 0.0
 
                 for row in operations:
-                    optcost = opcost + row[3] * row[4]
+                    opcost = opcost + row[3] * row[4]
                 
                 unit_cost.append((total_cost + opcost) / mo_qty)
-
-            values['real_cost'] = unit_cost[0]
+            
+            if unit_cost:
+                values['prod_cost'] = unit_cost[0]
 
         res = super(MrpProduction, self).write(values)
         return res
@@ -44,7 +66,7 @@ class MrpProduction(models.Model):
         #New
         prod_cost = values.get('product_id')
         prod_cost = self.env['product.product'].browse(prod_cost)
-        values['original_product_price'] = prod_cost.standard_price
+        values['std_cost'] = prod_cost.standard_price
         
         res = super(MrpProduction, self).create(values)
         return res
